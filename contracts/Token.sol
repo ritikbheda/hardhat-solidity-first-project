1c55f9e89f374cb6e8e34b55e9610ad5611ae427
Ritik Bheda <ritik.bheda@gmail.com>
2021-08-02 11:40:19 -0400

    deploys to local

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
+import "hardhat/console.sol";
+
 contract Token{
     string public name = "My hardhat Token";
     string public symbol = "MHT";
@@ -18,6 +20,10 @@ contract Token{
     }
 
     function transfer(address to, uint256 amount) external {
+
+        console.log("Sender balance is %s tokens", balances[msg.sender]);
+        console.log("Trying to send %s tokens to %s", amount, to);
+
         require((balances[msg.sender]) >= amount, "Not enough tokens");
         balances[msg.sender] -= amount;
         balances[to] += amount;
